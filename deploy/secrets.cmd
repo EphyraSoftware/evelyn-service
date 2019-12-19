03562faf8a15aa4621eb2ed1bda5ccce9fd884fa
ThetaSinner <greatdjonfire@hotmail.co.uk>
2019-12-19 01:16:05 +0000

    Add TLS to the group service

--- deploy/secrets.cmd
+++ deploy/secrets.cmd
@@ -8,15 +8,18 @@ rem Copy the trust store.
 copy \\nas.evelyn.internal\terraform\.files\bundles\truststore.p12 truststore.p12
 
 rem Copy the key stores.
-copy \\nas.evelyn.internal\terraform\.files\bundles\profile-service-keystore.p12 profile-service-keystore.p12
 copy \\nas.evelyn.internal\terraform\.files\bundles\web-entry-point-keystore.p12 web-entry-point-keystore.p12
+copy \\nas.evelyn.internal\terraform\.files\bundles\profile-service-keystore.p12 profile-service-keystore.p12
+copy \\nas.evelyn.internal\terraform\.files\bundles\group-service-keystore.p12 group-service-keystore.p12
 
 rem Remove the existing secrets.
-kubectl -n evelyn-services delete secret profile-service-keystore
 kubectl -n evelyn-services delete secret web-entry-point-keystore
+kubectl -n evelyn-services delete secret profile-service-keystore
+kubectl -n evelyn-services delete secret group-service-keystore
 
 rem Create the secrets.
-kubectl -n evelyn-services create secret generic profile-service-keystore --from-file=./profile-service-keystore.p12 --from-file=./truststore.p12
 kubectl -n evelyn-services create secret generic web-entry-point-keystore --from-file=./web-entry-point-keystore.p12 --from-file=./truststore.p12
+kubectl -n evelyn-services create secret generic profile-service-keystore --from-file=./profile-service-keystore.p12 --from-file=./truststore.p12
+kubectl -n evelyn-services create secret generic group-service-keystore --from-file=./group-service-keystore.p12 --from-file=./truststore.p12
 
 popd
