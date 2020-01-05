cdb1fff365ee023e7095b5a10c1b2ec6d4f25b50
ThetaSinner <greatdjonfire@hotmail.co.uk>
2020-01-05 22:59:42 +0000

    Tidy up gradle wrapper usage and remove remaining deployment iac

--- services/calendar/run.cmd
+++ services/calendar/run.cmd
@@ -1,5 +1,2 @@
 call ..\..\gradlew buildimage
 call ..\..\gradlew githubdockerpush
-call kubectl -n evelyn-services delete -f deploy.yml
-call kubectl -n evelyn-services apply -f deploy.yml
-call kubectl -n evelyn-services get pods
--- setup.sh
+++
@@ -1,2 +0,0 @@
-kubectl apply -f deploy.yaml
-kubectl create secret docker-registry ghregcred --namespace evelyn-services --docker-server=https://docker.pkg.github.com/ephyrasoftware/evelyn-service --docker-username=ThetaSinner --docker-password=<your-pword> --docker-email=greatdjonfire@hotmail.co.uk
