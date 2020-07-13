0e9110c9e6d6ddcf0ca520c5620cc347c74102e4
ThetaSinner <greatdjonfire@hotmail.co.uk>
2020-07-14 00:20:15 +0100

    Upgrade task service dependencies and migrate to resourceserver oauth

--- services/task/rest-api/src/main/java/org/evelyn/services/task/rest/api/TaskController.java
+++ services/task/rest-api/src/main/java/org/evelyn/services/task/rest/api/TaskController.java
@@ -1,8 +1,11 @@
 package org.evelyn.services.task.rest.api;
 
+import org.evelyn.library.authentication.token.TokenLookup;
 import org.evelyn.services.task.api.ITaskService;
 import org.evelyn.services.task.api.model.CreateTaskRequest;
 import org.evelyn.services.task.api.model.Task;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,42 +18,46 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.json.Json;
-import javax.json.JsonArray;
 import java.io.StringReader;
-import java.security.Principal;
 import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
 public class TaskController {
     private final ITaskService taskService;
+    private final TokenLookup tokenLookup;
 
-    public TaskController(ITaskService taskService) {
+    public TaskController(ITaskService taskService, TokenLookup tokenLookup) {
         this.taskService = taskService;
+        this.tokenLookup = tokenLookup;
     }
 
     @PostMapping(value = "/tasks")
     @ResponseBody
-    public Task createTask(final Principal principal, @RequestBody @Validated CreateTaskRequest createTaskRequest) {
-        return taskService.createTask(principal.getName(), createTaskRequest);
+    public Task createTask(@AuthenticationPrincipal Jwt principal, @RequestBody @Validated CreateTaskRequest createTaskRequest) {
+        var tokenInfo = tokenLookup.getTokenInfo(principal);
+        return taskService.createTask(tokenInfo.getSubject(), createTaskRequest);
     }
 
     @GetMapping(value = "/tasks")
     @ResponseBody
-    public List<Task> getTasks(final Principal principal) {
-        return taskService.getTasks(principal.getName());
+    public List<Task> getTasks(@AuthenticationPrincipal Jwt principal) {
+        var tokenInfo = tokenLookup.getTokenInfo(principal);
+        return taskService.getTasks(tokenInfo.getSubject());
     }
 
     @PatchMapping(value = "/tasks/{taskId}")
     @ResponseBody
-    public Task patchTask(final Principal principal, @PathVariable String taskId, @RequestBody String patchOperations) {
-        JsonArray jsonArray = Json.createReader(new StringReader(patchOperations)).readArray();
-        return taskService.patchTask(principal.getName(), taskId, Json.createPatch(jsonArray));
+    public Task patchTask(@AuthenticationPrincipal Jwt principal, @PathVariable String taskId, @RequestBody String patchOperations) {
+        var jsonArray = Json.createReader(new StringReader(patchOperations)).readArray();
+        var tokenInfo = tokenLookup.getTokenInfo(principal);
+        return taskService.patchTask(tokenInfo.getSubject(), taskId, Json.createPatch(jsonArray));
     }
 
     @DeleteMapping(value = "/tasks/{taskId}")
     @ResponseBody
-    public void deleteTask(final Principal principal, @PathVariable String taskId) {
-        taskService.deleteTask(principal.getName(), taskId);
+    public void deleteTask(@AuthenticationPrincipal Jwt principal, @PathVariable String taskId) {
+        var tokenInfo = tokenLookup.getTokenInfo(principal);
+        taskService.deleteTask(tokenInfo.getSubject(), taskId);
     }
 }
